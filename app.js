8ac6a3c0c4320540ce7f6c099e6d094a43afa8df
System Administrator <root@Karis-MacBook-Air.local>
2017-04-25 12:22:02 +1000

    make adding to dos possible

---
+++ app.js
@@ -0,0 +1,15 @@
+
+//console.log("I love Sean");
+
+//alert("hello woofer");
+
+
+function submitTodo() {
+  var item = document.getElementById('newItem').value
+  var tasks = document.getElementById('tasks')
+  var nodeItem = document.createTextNode(item)
+  var listItem = document.createElement("li")
+  listItem.appendChild(nodeItem)
+  tasks.appendChild(listItem)   // Adds new item to the list
+  document.getElementById('newItem').value = ""   // Clears the text input box
+}
