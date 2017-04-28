37c3ab702ad9443cf556b4fbae5a80b933109d8e
Kari Matthews <karimatthews3@gmail.com>
2017-04-28 13:27:52 +1000

    Add delete button

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
+
+
 function submitTodo() {
   var item = document.getElementById('newItem').value
   if (item == "") {
-    return; 
-  }
+    return;
+    }
   var tasksContainer = document.getElementById('tasks')
   var nodeItem = document.createTextNode(item)
-
   // create checkbox for list element
   var checkbox = document.createElement("input")
   checkbox.type = 'checkbox'
-
+  //create delete button for list item
+  var deleteButton = document.createElement("button")
+  deleteButton.type = 'button'
+  // deleteButton.onclick("deleteItem()") // maybe triggers deleting the item???
   var listItem = document.createElement("p")
   listItem.appendChild(checkbox) // add checkbox to list element
   listItem.appendChild(nodeItem)  // Adds text to list element
-
+  listItem.appendChild(deleteButton)  // Adds delete button to element
   tasksContainer.appendChild(listItem)   // Adds new item to the list
 
   document.getElementById('newItem').value = ""   // Clears the text input box
-}
 
+  deleteButton.addEventListener('click', function(event) {
+          tasksContainer.removeChild(listItem);
+      });
+}
 
 function checkForEnter(event) { // when we type into the textbox, submit the todo if we're hitting Enter
   if (event.keyCode == 13) {
